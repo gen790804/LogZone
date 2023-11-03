19107d16efe34ae8c12f1c8916e0cb846669fcda
log <log@gis.tw>
2023-11-03 14:26:48 +0800

    1.測試

---
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1
+{
+    public class HomeController : Controller
+    {
+        // Fields
+        private readonly WorkContext _workContext = null;
+
+
+        // Constructors
+        public HomeController(WorkContext workContext)
+        {
+            // Default
+            _workContext = workContext;
+        }
+
+
+        // Methods
+        public ActionResult Index()
+        {
+            // ViewBag
+            this.ViewBag.Message = _workContext.GetValue();
+
+            // Return
+            return View();
+        }
+    }
+}
